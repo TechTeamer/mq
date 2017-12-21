596d55850068a2f3cdd0d4f5e44b00d991f3a6e6
jnikolov <nikolov.jd@gmail.com>
2017-12-21 15:49:22 +0100

    removed done parameter from describe

--- test/QueueClient.test.js
+++ test/QueueClient.test.js
@@ -9,7 +9,7 @@ let expect = chai.expect
 let channelName = 'queue-client-queue-server'
 let message = 'hi'
 
-describe('QueueClient && QueueServer', (done) => {
+describe('QueueClient && QueueServer', () => {
   it('QueueClient.send() sends a message and QueueServer.consume() receives it', (done) => {
     const producer = fork('./test/fixtures/queue-client-producer.js', [channelName, message])
     const consumer = fork('./test/fixtures/queue-server-consumer.js', [channelName])
