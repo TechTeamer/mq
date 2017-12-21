21cd5249318acfe954685a1e206b62d20efba416
jnikolov <nikolov.jd@gmail.com>
2017-12-21 15:47:01 +0100

    Client -> Server message test

--- test/QueueClient.test.js
+++ test/QueueClient.test.js
@@ -6,17 +6,19 @@ chai.use(chaiAsPromised)
 
 let expect = chai.expect
 
-let channelName = 'test-channel'
-let message = 'test-message'
+let channelName = 'queue-client-queue-server'
+let message = 'hi'
 
 describe('QueueClient && QueueServer', (done) => {
-  it('QueueClient.send() sends a message and QueueServer.consume() receives it', () => {
+  it('QueueClient.send() sends a message and QueueServer.consume() receives it', (done) => {
     const producer = fork('./test/fixtures/queue-client-producer.js', [channelName, message])
     const consumer = fork('./test/fixtures/queue-server-consumer.js', [channelName])
 
     consumer.on('message', (msg) => {
-      if (msg === message){
+      if (msg === message) {
         done()
+      } else {
+        done(new Error('msg !== message'))
       }
     })
   })
