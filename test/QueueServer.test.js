502fcc6aca482910dda85688bc66ce79ba3d581f
jnikolov <nikolov.jd@gmail.com>
2017-12-21 12:25:26 +0100

    QueueConnection test

--- test/QueueServer.test.js
+++ test/QueueServer.test.js
@@ -1,13 +1,5 @@
-const path = require('path')
-const testProcess = require('./fixtures/test-process')
-const {callCommand} = testProcess(path.resolve('./fixtures/test-queue-server'))
 
 describe('QueueMessage', () => {
   it('can connect', () => {
-    return callCommand('connect').then((result) => {
-      console.log('connect result', result)
-    }).catch((err) => {
-      console.log('connect error', err)
-    })
   })
 })
