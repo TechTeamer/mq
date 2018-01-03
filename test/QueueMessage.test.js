df8a7eb8fe85f0c5a6d2ff01e56823f5f3bbe2c3
jnikolov <nikolov.jd@gmail.com>
2018-01-03 11:03:00 +0100

    QueueMessage test

---
+++ test/QueueMessage.test.js
@@ -0,0 +1,28 @@
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
+
+chai.use(chaiAsPromised)
+
+let expect = chai.expect
+
+const QueueMessage = require('../src/QueueMessage')
+
+describe('QueueMessage', () => {
+  let status = 'ok'
+  let data = 'This is a valid QueueMessage'
+
+  let badQueueMessage = 'thisIsNotAQueueMessage'
+  let goodQueueMessage = JSON.stringify(new QueueMessage(status, data))
+
+  it('#fromJSON() returns a QueueMessage with status "error" if it receives an invalid QueueMessage', () => {
+    return expect((QueueMessage.fromJSON(badQueueMessage)).status).to.equal('error')
+  })
+
+  it('#fromJSON() parses the stringified QueueMessage with the correct status', () => {
+    return expect((QueueMessage.fromJSON(goodQueueMessage)).status).to.equal(status)
+  })
+
+  it('#fromJSON() parses the stringified QueueMessage with the correct data', () => {
+    return expect((QueueMessage.fromJSON(goodQueueMessage)).data).to.equal(data)
+  })
+})
