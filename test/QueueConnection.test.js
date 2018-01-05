07be025a155604d19b5e2e83fcfda97030e2eaaf
jnikolov <nikolov.jd@gmail.com>
2018-01-05 13:54:37 +0100

    fixed loading of config file in QueueConnection

--- test/QueueConnection.test.js
+++ test/QueueConnection.test.js
@@ -1,7 +1,25 @@
 const QueueConnection = require('../src/QueueConnection')
+const fs = require('fs')
+let config
+
+if (fs.existsSync('./fixtures/TestConfig.js')) {
+  config = require('./fixtures/TestConfig')
+} else {
+  let QueueConfig = require('../src/QueueConfig')
+  config = new QueueConfig({
+    url: 'amqps://localhost:5671',
+    options: {
+      rejectUnauthorized: false,
+      cert: '',
+      key: '',
+      ca: []
+    },
+    rpcTimeoutMs: 10000,
+    rpcQueueMaxSize: 100
+  })
+}
 
 describe('QueueConnection', () => {
-  let config = require('./fixtures/TestConfig')
   let connection = new QueueConnection(config)
   it('#connect() creates a connection to RabbitMQ', (done) => {
     connection.connect()
