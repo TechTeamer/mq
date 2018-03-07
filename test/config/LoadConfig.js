c026efb671df68427e19513d6b2f0afffc7e85f4
tunderdomb <tunderdomb@gmail.com>
2018-03-07 15:11:18 +0100

    add helper message to local config loading

--- test/config/LoadConfig.js
+++ test/config/LoadConfig.js
@@ -3,7 +3,12 @@ let config
 if (process.env.NODE_ENV === 'travis') {
   config = require('./DefaultConfig')
 } else {
-  config = require('./TestConfig')
+  try {
+    config = require('./TestConfig')
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.warn(`Please rename "test/fixtures/TestConfig.js.config" to "TestConfig.js" and fill in the configuration data.`)
+  }
 }
 
 module.exports = config
