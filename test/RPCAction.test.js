09b21d74ad71050a1aabbd3f787ca2c39db52c02
tgabi333 <tgabi333@gmail.com>
2019-10-29 18:50:40 +0100

    more tests

--- test/RPCAction.test.js
+++ test/RPCAction.test.js
@@ -1,6 +1,8 @@
 const QueueManager = require('../src/QueueManager')
 const ConsoleInspector = require('./consoleInspector')
 const config = require('./config/LoadConfig')
+const chai = require('chai')
+const expect = chai.expect
 
 describe('RPCClient && RPCServer actions', function () {
   const rpcName = 'test-rpc-action'
@@ -34,6 +36,38 @@ describe('RPCClient && RPCServer actions', function () {
 
     rpcClient.callAction('compareString', stringMessage, 1000).catch((err) => {
       done(err)
+    })
+  })
+
+  it('RPCClient handles unserializeable content', (done) => {
+    rpcServer.registerAction('wrongtest1', (msg) => {
+      done(new Error('RPCServer Action should not be called'))
+    })
+
+    const obj = {}
+    obj.a = { b: obj }
+
+    rpcClient.callAction('wrongtest1', obj, 1000).then(() => {
+      done('RPCClient successfully sent unsendable data')
+    }).catch((err) => {
+      expect(err).to.be.an.instanceof(Error)
+      done()
+    })
+  })
+
+  it('RPCServer handles unserializeable content', (done) => {
+    rpcServer.registerAction('wrongtest2', (msg) => {
+      const obj = {}
+      obj.a = { b: obj }
+
+      return obj
+    })
+
+    rpcClient.callAction('wrongtest2', 'hello', 1000).then(() => {
+      done('RPCServer successfully sent unsendable data')
+    }).catch((err) => {
+      expect(err).to.be.an.instanceof(Error)
+      done()
     })
   })
 })
