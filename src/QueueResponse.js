6dce93f7b3c498a6c21cf151af807fa90e75695c
tunderdomb <tunderdomb@gmail.com>
2021-04-23 16:08:11 +0200

    queue response status (#25)

--- src/QueueResponse.js
+++ src/QueueResponse.js
@@ -1,6 +1,54 @@
+const OK = 'OK'
+const NOT_FOUND = 'NOT_FOUND'
+const ERROR = 'ERROR'
+const statuses = {
+  [OK]: OK,
+  [NOT_FOUND]: NOT_FOUND,
+  [ERROR]: ERROR
+}
+
 class QueueResponse {
   constructor () {
+    this.statusMessage = ''
+    this.statusCode = ''
     this.attachments = new Map()
+  }
+
+  get statuses () {
+    return statuses
+  }
+
+  get OK () {
+    return OK
+  }
+
+  get NOT_FOUND () {
+    return NOT_FOUND
+  }
+
+  get ERROR () {
+    return ERROR
+  }
+
+  /**
+   * @param statusCode
+   * @param [statusMessage]
+   */
+  setStatus (statusCode, statusMessage = this.statuses[statusCode]) {
+    this.statusCode = statusCode
+    this.statusMessage = statusMessage
+  }
+
+  ok (statusMessage) {
+    this.setStatus(this.OK, statusMessage)
+  }
+
+  notFound (statusMessage) {
+    this.setStatus(this.NOT_FOUND, statusMessage)
+  }
+
+  error (statusMessage) {
+    this.setStatus(this.ERROR, statusMessage)
   }
 
   /**
