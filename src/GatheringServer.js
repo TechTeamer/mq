e20cc9a6183e246b368b38a50c17506d9d4bb890
tunderdomb <tunderdomb@gmail.com>
2021-04-29 14:23:32 +0200

    RPC gathering (#26)

---
+++ src/GatheringServer.js
@@ -0,0 +1,242 @@
+const QueueMessage = require('./QueueMessage')
+const QueueResponse = require('./QueueResponse')
+
+class GatheringServer {
+  /**
+   * @param {QueueConnection} queueConnection
+   * @param {Console} logger
+   * @param {String} name
+   * @param {Object} options
+   */
+  constructor (queueConnection, logger, name, options) {
+    this._connection = queueConnection
+    this._logger = logger
+    this.name = name
+    this.statusQueue = name
+
+    const { prefetchCount, timeoutMs } = options || {}
+    this._prefetchCount = prefetchCount
+    this._responseTimeoutMs = timeoutMs
+
+    this.actions = new Map()
+  }
+
+  async initialize () {
+    try {
+      const channel = await this._connection.getChannel()
+      await channel.assertExchange(this.name, 'fanout', { durable: true })
+      const serverQueue = await channel.assertQueue('', { exclusive: true })
+      const serverQueueName = serverQueue.queue
+
+      await channel.prefetch(this._prefetchCount)
+      await channel.bindQueue(serverQueueName, this.name, '')
+
+      await channel.consume(serverQueueName, (msg) => {
+        this._handleGatheringAnnouncement(channel, msg).catch(() => {
+          // this should not throw
+        })
+      })
+
+      await channel.assertQueue(this.statusQueue, {
+        exclusive: false,
+        autoDelete: true
+      })
+      await channel.consume(this.statusQueue, () => {
+        // noop
+      }, { noAck: true })
+    } catch (err) {
+      this._logger.error('CANNOT INITIALIZE QUEUE GATHERING SERVER', this.name, err)
+      throw new Error('Error initializing Gathering Server')
+    }
+  }
+
+  /**
+   * @param {*} msg
+   * @param {QueueMessage} request
+   * @param {QueueResponse} response
+   * @protected
+   * @returns {Promise}
+   */
+  async _callback (msg, request, response) {
+    const {
+      action,
+      data
+    } = msg || {}
+    if (!this.actions.has(action)) {
+      response.notFound(`No action handler for ${action}`)
+      return
+    }
+
+    const handler = this.actions.get(action)
+
+    return handler.call(this, data, request, response)
+  }
+
+  /**
+   * @param {string} action
+   * @param {Function} handler
+   */
+  registerAction (action, handler) {
+    if (typeof handler !== 'function') {
+      throw new TypeError(`${typeof handler} is not a Function`)
+    } else if (this.actions.has(action)) {
+      this._logger.warn(`Actions-handlers map already contains an action named ${action}`)
+    } else {
+      this.actions.set(action, handler)
+    }
+  }
+
+  /**
+   * @param {Function} cb
+   */
+  consume (cb) {
+    this._callback = cb
+  }
+
+  /**
+   * @param channel
+   * @param msg
+   * @return {Promise}
+   * @private
+   */
+  async _handleGatheringAnnouncement (channel, msg) {
+    if (!msg || !msg.properties) {
+      this._logger.error(`QUEUE GATHERING SERVER: INVALID REQUEST ON '${this.name}': NO MESSAGE/PROPERTIES`, msg)
+      this._nack(channel, msg)
+      return
+    }
+    if (!msg.properties.correlationId) {
+      this._logger.error(`QUEUE GATHERING SERVER: INVALID REQUEST ON '${this.name}': NO CORRELATION ID`, msg)
+      this._nack(channel, msg)
+      return
+    }
+    if (!msg.properties.replyTo) {
+      this._logger.error(`QUEUE GATHERING SERVER: INVALID REQUEST ON '${this.name}': NO REPLY TO`, msg)
+      this._nack(channel, msg)
+      return
+    }
+
+    const correlationId = msg.properties.correlationId
+    const replyTo = msg.properties.replyTo
+    let request
+
+    try {
+      request = QueueMessage.unserialize(msg.content)
+      if (request.status !== 'ok') {
+        this._logger.error(`QUEUE GATHERING SERVER: MESSAGE NOT OK '${this.name}' ${correlationId}`, request)
+        this._sendStatus(channel, replyTo, correlationId, 'error', 'message not OK')
+        this._nack(channel, msg)
+        return
+      }
+    } catch (err) {
+      this._logger.error(`QUEUE GATHERING SERVER: MALFORMED MESSAGE '${this.name}' ${correlationId}`, err)
+      this._sendStatus(channel, replyTo, correlationId, 'error', 'malformed message')
+      this._nack(channel, msg)
+      return
+    }
+
+    let responseTimedOut = false
+    const response = new QueueResponse()
+    const timeoutMs = typeof request.timeOut === 'number' ? request.timeOut : this._responseTimeoutMs
+    const timerId = setTimeout(() => {
+      responseTimedOut = true
+      this._logger.error(`QUEUE GATHERING SERVER: RESPONSE TIMED OUT '${this.name}' ${correlationId}`)
+      this._sendStatus(channel, replyTo, correlationId, 'ok', 'response timed out')
+      this._nack(channel, msg)
+    }, timeoutMs)
+
+    let answer
+    try {
+      answer = await this._callback(request.data, request, response)
+    } catch (err) {
+      this._logger.error(`QUEUE GATHERING SERVER: response failed '${this.name}' ${correlationId}`)
+      this._sendStatus(channel, replyTo, correlationId, 'error', 'response failed')
+      this._nack(channel, msg)
+      return
+    }
+
+    if (responseTimedOut) {
+      return
+    }
+
+    clearTimeout(timerId)
+
+    if (!response.statusCode) {
+      // implicit status
+      if (typeof answer === 'undefined') {
+        response.setStatus(response.NOT_FOUND)
+      } else {
+        response.setStatus(response.OK)
+      }
+    }
+
+    let reply
+    try {
+      if (response.statusCode === response.OK) {
+        const replyAttachments = response.getAttachments()
+        const queueMessage = new QueueMessage('ok', answer)
+        if (replyAttachments instanceof Map) {
+          for (const [key, value] of replyAttachments) {
+            queueMessage.addAttachment(key, value)
+          }
+        }
+        reply = queueMessage.serialize()
+      } else if (response.statusCode === response.NOT_FOUND) {
+        this._sendStatus(channel, replyTo, correlationId, 'ok', 'not found')
+        this._nack(channel, msg)
+        return
+      } else if (response.statusCode === response.ERROR) {
+        const queueMessage = new QueueMessage('error', response.statusMessage)
+        reply = queueMessage.serialize()
+      }
+    } catch (err) {
+      this._logger.error('QUEUE GATHERING SERVER: Failed to construct reply', this.name, err)
+      this._sendStatus(channel, replyTo, correlationId, 'error', 'failed to construct reply')
+      this._nack(channel, msg)
+      return
+    }
+
+    try {
+      channel.sendToQueue(replyTo, reply, { correlationId: correlationId, type: 'reply' })
+      this._ack(channel, msg)
+    } catch (err) {
+      this._logger.error('QUEUE GATHERING SERVER: Failed to send reply', this.name, err)
+    }
+  }
+
+  _sendStatus (channel, replyTo, correlationId, status, message = '') {
+    const reply = new QueueMessage(status, message)
+    channel.sendToQueue(replyTo, reply.serialize(), { correlationId, type: 'status' })
+  }
+
+  /**
+   * @param ch
+   * @param msg
+   * @private
+   */
+  _ack (ch, msg) {
+    if (msg.acked) {
+      this._logger.error('trying to double ack', msg)
+      return
+    }
+    ch.ack(msg)
+    msg.acked = true
+  }
+
+  /**
+   * @param channel
+   * @param msg
+   * @param [requeue=true]
+   * @private
+   */
+  _nack (channel, msg, requeue = false) {
+    if (msg.acked) {
+      this._logger.error('trying to double nack', msg)
+      return
+    }
+    channel.nack(msg, false, requeue)
+    msg.acked = true
+  }
+}
+
+module.exports = GatheringServer
---
+++ test/Gathering-multi-server.test.js
@@ -0,0 +1,157 @@
+const QueueManager = require('../src/QueueManager')
+const GatheringServer = require('../src/GatheringServer')
+const ConsoleInspector = require('./consoleInspector')
+const config = require('./config/LoadConfig')
+
+describe('GatheringClient && multiple GatheringServer', () => {
+  const gatheringName = 'test-gathering-multi'
+  const logger = new ConsoleInspector(console)
+  const timeoutMs = 1000
+
+  const queueManager = new QueueManager(config)
+  queueManager.setLogger(logger)
+
+  const gatheringClient = queueManager.getGatheringClient(gatheringName, { queueMaxSize: 100, timeoutMs })
+  const gatheringServer1 = new GatheringServer(queueManager.connection, queueManager._logger, gatheringName, { prefetchCount: 1, timeoutMs })
+  const gatheringServer2 = new GatheringServer(queueManager.connection, queueManager._logger, gatheringName, { prefetchCount: 1, timeoutMs })
+
+  before(() => {
+    return queueManager.connect().then(() => {
+      return gatheringServer1.initialize()
+    }).then(() => {
+      return gatheringServer2.initialize()
+    })
+  })
+
+  after(() => {
+    logger.empty()
+  })
+
+  it('GatheringClient.request() sends something and one of the GatheringServers reply in time with setting ok response status', (done) => {
+    const messageBody = 'hello'
+    gatheringServer1.consume((msg, message, response) => {
+      response.setStatus(response.NOT_FOUND)
+    })
+    gatheringServer2.consume((msg, message, response) => {
+      response.setStatus(response.OK)
+      return msg
+    })
+
+    gatheringClient.request(messageBody, 10000).then((res) => {
+      done()
+    }).catch((err) => {
+      done(err)
+    })
+  })
+
+  it('GatheringClient.request() sends something and one of the GatheringServers reply in time with implied ok response status', (done) => {
+    const messageBody = 'hello'
+    gatheringServer1.consume((msg, message, response) => {
+      response.setStatus(response.NOT_FOUND)
+    })
+    gatheringServer2.consume((msg) => {
+      return msg
+    })
+
+    gatheringClient.request(messageBody, 10000).then((res) => {
+      done()
+    }).catch((err) => {
+      done(err)
+    })
+  })
+
+  it('GatheringClient.request() sends something and every GatheringServer replies with not_found status', (done) => {
+    const messageBody = 'hello'
+    gatheringServer1.consume((msg, message, response) => {
+      response.setStatus(response.NOT_FOUND)
+    })
+    gatheringServer2.consume((msg, message, response) => {
+      response.setStatus(response.NOT_FOUND)
+    })
+
+    gatheringClient.request(messageBody, 10000).then((res) => {
+      done()
+    }).catch((err) => {
+      done(err)
+    })
+  })
+
+  it('GatheringClient.request() sends something and every GatheringServer replies with ok status', (done) => {
+    const stringMessage = 'hello'
+    gatheringServer1.consume(() => {
+      return stringMessage
+    })
+    gatheringServer2.consume(() => {
+      return stringMessage
+    })
+
+    gatheringClient.request(stringMessage, 10000).then((msg) => {
+      if (msg === stringMessage) {
+        done()
+      } else {
+        done(new Error('String received is not the same as the String sent'))
+      }
+      return true
+    }).catch((err) => {
+      done(err)
+    })
+  })
+
+  it('GatheringServer.consume() undefined reply implies not_found status and request resolves with undefined', (done) => {
+    const messageBody = 'hello'
+    gatheringServer1.consume(() => {
+      // undefined is implied not found
+    })
+    gatheringServer2.consume(() => {
+      // undefined is implied not found
+    })
+
+    gatheringClient.request(messageBody, 10000).then((res) => {
+      if (typeof res === 'undefined') {
+        done()
+      } else {
+        done(new Error('Should have resolved with undefined'))
+      }
+    }).catch((err) => {
+      done(err)
+    })
+  })
+
+  it('gatheringClient.request(acceptNotFound=false) rejects not found statuses', (done) => {
+    const messageBody = 'hello'
+    gatheringServer1.consume(() => {
+      // undefined is implied not found
+    })
+    gatheringServer2.consume(() => {
+      // undefined is implied not found
+    })
+
+    gatheringClient.request(messageBody, 10000, null, false, false).then((res) => {
+      done(new Error('Should reject not found with acceptNotFound=false'))
+    }).catch(() => {
+      done()
+    })
+  })
+
+  it('gatheringClient.request() rejects when all consumers time out', (done) => {
+    const messageBody = 'hello'
+    gatheringServer1.consume(() => {
+      // undefined is implied not found
+      return new Promise(resolve => setTimeout(resolve, 500))
+    })
+    gatheringServer2.consume(() => {
+      // undefined is implied not found
+      return new Promise(resolve => setTimeout(resolve, 500))
+    })
+
+    gatheringClient.request(messageBody, 100, null, false, false).then((res) => {
+      done(new Error('Should reject'))
+    }).catch((err) => {
+      if (err.message.includes('QUEUE GATHERING RESPONSE TIMED OUT')) {
+        done()
+      } else {
+        done(err)
+      }
+    })
+  })
+})
