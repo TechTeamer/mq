c618de1a6ae7f4d6405121d63b93d01139236989
tunderdomb <tunderdomb@gmail.com>
2022-06-22 12:57:32 +0200

    add ProtoQueueMessage

---
+++ src/ProtoQueueMessage.js
@@ -0,0 +1,55 @@
+const QueueMessage = require('./QueueMessage')
+
+class ProtoQueueMessage extends QueueMessage {
+  static load (protoPath, definitionLookup) {
+    try {
+      const protobuf = require('protobufjs')
+      const proto = protobuf.load(protoPath)
+
+      if (!proto) {
+        return null
+      }
+      if (!definitionLookup) {
+        return proto
+      }
+      if (definitionLookup) {
+        return proto.lookup(definitionLookup)
+      }
+    } catch (err) {
+      return null
+    }
+  }
+
+  serialize () {
+    const errorMessage = this.ContentSchema.verify(this.data)
+
+    if (errorMessage) {
+      throw new Error(`Error verifying ProtoQueueMessage data: ${errorMessage}`)
+    }
+
+    const message = this.ContentSchema.create(this.data)
+
+    return this.ContentSchema.encode(message).finish()
+  }
+
+  static unserialize (buffer, ContentSchema) {
+    try {
+      const message = ContentSchema.decode(buffer)
+      const data = ContentSchema.toObject(message, {
+        enums: String, // enums as string names
+        longs: Number, // longs as strings (requires long.js)
+        bytes: Buffer, // bytes as base64 encoded strings
+        defaults: true, // includes default values
+        arrays: true, // populates empty arrays (repeated fields) even if defaults=false
+        objects: true, // populates empty objects (map fields) even if defaults=false
+        oneofs: true // includes virtual oneof fields set to the present field's name
+      })
+
+      return new ProtoQueueMessage('ok', data, null, ContentSchema)
+    } catch (err) {
+      return new ProtoQueueMessage('error', `Cannot decode protobuf: ${err.message}`, null, ContentSchema)
+    }
+  }
+}
+
+module.exports = ProtoQueueMessage
---
+++ test/ProtoQueueMessage.test.js
@@ -0,0 +1,59 @@
+const chai = require('chai')
+const assert = chai.assert
+const expect = chai.expect
+const path = require('path')
+const protobuf = require('protobufjs')
+
+const QueueMessage = require('../src/QueueMessage')
+const ProtoQueueMessage = require('../src/ProtoQueueMessage')
+
+describe('ProtoQueueMessage', () => {
+  const okStatus = 'ok'
+
+  const number = 1
+  const string = 'hello ŰÁÉÚŐÓÜÖÍűáéúőóüöí$\\`#^+-[]<>*;~!%/()孫詒讓\u1FFF\u{10FFFF}'
+  const array = [1, 2, 3]
+  const testBuffer = Buffer.from('test buffer')
+  const testData = { number, string, array, buffer: testBuffer }
+  let TestMessage
+
+  before(async () => {
+    const RootProtobuf = await protobuf.load(path.join(process.cwd(), 'test/contentSchemas/protobuf_test_schema.proto'))
+    TestMessage = RootProtobuf.lookup('TestMessage')
+  })
+
+  it('#fromJSON() returns a QueueMessage instance', () => {
+    assert.instanceOf(ProtoQueueMessage.fromJSON({}), QueueMessage)
+  })
+
+  it('#serialize() serialize the ProtoQueueMessage to a Buffer', () => {
+    const queueMessage = new ProtoQueueMessage(okStatus, testData, 100, TestMessage)
+    queueMessage.addAttachment('testBuffer', Buffer.from('testBuffer'))
+    const queueMessageBuffer = queueMessage.serialize()
+    expect(queueMessageBuffer).to.be.instanceof(Buffer)
+  })
+
+  it('#unserialize() deserialize the QueueMessage to the original QueueMessage', () => {
+    const queueMessage = new ProtoQueueMessage(okStatus, testData, 100, TestMessage)
+    queueMessage.addAttachment('testBuffer', Buffer.from('testBuffer'))
+    const queueMessageBuffer = queueMessage.serialize()
+    const newQueueMessage = ProtoQueueMessage.unserialize(queueMessageBuffer, TestMessage)
+    assert.strictEqual(newQueueMessage.status, okStatus, 'status not match')
+
+    const data = newQueueMessage.data
+    assert.isObject(data, 'not an object')
+    assert.hasAllKeys(data, ['number', 'string', 'array', 'buffer'], 'object members not match')
+    assert.strictEqual(data.number, number, 'number not match')
+    assert.strictEqual(data.string, string, 'string not match')
+    assert.isArray(data.array, array, 'not an array')
+    assert.sameMembers(data.array, array, 'array members not match')
+    const buffer = Buffer.from(data.buffer)
+    assert.strictEqual(buffer.toString('utf8'), 'test buffer', 'buffer content not match')
+
+    assert.strictEqual(newQueueMessage.timeOut, null, 'timeout not match')
+
+    newQueueMessage.getAttachments().forEach((value, key) => {
+      assert.strictEqual(value.toString(), queueMessage.getAttachments().get(key).toString(), 'attachment not match')
+    })
+  })
+})
