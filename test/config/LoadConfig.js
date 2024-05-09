c1ed8fb055d289e81116f49fec18f52eea4beb7f
Szecsődi Imre <imre.szecsodi@facekom.com>
2024-05-09 16:41:51 +0000

    replace no missing require with import version

--- test/config/LoadConfig.js
+++ test/config/LoadConfig.js
@@ -4,7 +4,7 @@ if (process.env.NODE_ENV === 'travis') {
   config = (await import('./DefaultConfig.js')).default
 } else {
   try {
-    config = (await import('./TestConfig.js')).default // eslint-disable-line n/no-missing-require
+    config = (await import('./TestConfig.js')).default // eslint-disable-line n/no-missing-import
   } catch (e) {
     // eslint-disable-next-line no-console
     console.warn('Please rename "test/fixtures/TestConfig.js.config" to "TestConfig.js" and fill in the configuration data.')
